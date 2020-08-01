fd33e771ab797873428cb4f214f551e30af499e5
vinay singh <svinaykumar70@gmail.com>
2020-08-01 11:58:04 +0530

    final commit

---
+++ app/src/main/java/com/vinay/covid_19/DetailActivity.java
@@ -0,0 +1,58 @@
+package com.vinay.covid_19;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private int positionCountry;
+    TextView tvCountry,tvCases,tvRecovered,tvCritical,tvActive,tvTodayCases,tvTotalDeaths,tvTodayDeaths,tvtodayRecovered;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        positionCountry = intent.getIntExtra("position",0);
+
+        getSupportActionBar().setTitle("Details of "+AffectedCountries.countryModelList.get(positionCountry).getCountry());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+        tvCountry = findViewById(R.id.tvCountry);
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvTotalDeaths = findViewById(R.id.tvDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvtodayRecovered = findViewById(R.id.tvtodayRecovered);
+
+        tvCountry.setText(AffectedCountries.countryModelList.get(positionCountry).getCountry());
+        tvCases.setText(AffectedCountries.countryModelList.get(positionCountry).getCases());
+        tvRecovered.setText(AffectedCountries.countryModelList.get(positionCountry).getRecovered());
+        tvCritical.setText(AffectedCountries.countryModelList.get(positionCountry).getCritical());
+        tvActive.setText(AffectedCountries.countryModelList.get(positionCountry).getActive());
+        tvTodayCases.setText(AffectedCountries.countryModelList.get(positionCountry).getTodayCases());
+        tvTotalDeaths.setText(AffectedCountries.countryModelList.get(positionCountry).getDeaths());
+        tvTodayDeaths.setText(AffectedCountries.countryModelList.get(positionCountry).getTodayDeaths());
+        tvtodayRecovered.setText(AffectedCountries.countryModelList.get(positionCountry).getTodayRecovered());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
